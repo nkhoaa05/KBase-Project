963f0dd4b4e39833bac5e0313b257c1c14b090ba
nkhoa <nkhoa3625@gmail.com>
2026-09-18 01:26:34 +0700

    configure spring security

--- src/main/java/org/example/kbase/Controllers/UserController.java
+++ src/main/java/org/example/kbase/Controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.example.kbase.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 }
