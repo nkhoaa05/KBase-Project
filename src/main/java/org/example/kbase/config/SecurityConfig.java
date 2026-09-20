1b9ebf528992ae2d7bd4537ee91ad1be410d5a85
nkhoa <nkhoa3625@gmail.com>
2026-09-20 18:36:23 +0700

    chore: add dependency for jwt implement

--- src/main/java/org/example/kbase/config/SecurityConfig.java
+++ src/main/java/org/example/kbase/config/SecurityConfig.java
@@ -1,15 +1,17 @@
-package org.example.kbase.Config;
+package org.example.kbase.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${api.prefix}")
@@ -21,7 +23,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -32,15 +34,15 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers(
-                                apiPrefix +  "/auth/register",
-                                        apiPrefix + "/auth/login",
-                                                    "/swagger-ui/**",
-                                                    "/swagger-ui/index.html",
-                                                    "/v3/api-docs/**"
-                        ).permitAll()
+                    .requestMatchers(
+                            apiPrefix +  "/auth/register",
+                                    apiPrefix + "/auth/login",
+                                                "/swagger-ui/**",
+                                                "/swagger-ui/index.html",
+                                                "/v3/api-docs/**"
+                    ).permitAll()
 
-                        .anyRequest().authenticated()
+                    .anyRequest().authenticated()
                 );
 
         return http.build();
